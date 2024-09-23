98fc5490806a80fdae01c64212ddbcce6e9e01ce
jjlee <jj76.lee@navercorp.com>
2024-09-24 00:30:35 +0900

    create InteractFun modify some names not proper

--- app/src/main/java/com/example/mycomposepractice/firstPractice/ComposeFeature.kt
+++ app/src/main/java/com/example/mycomposepractice/firstPractice/ComposeFeature.kt
@@ -1,10 +1,13 @@
 package com.example.mycomposepractice.firstPractice
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycomposepractice.ui.theme.MyComposePracticeTheme
 
@@ -18,13 +21,29 @@ fun DynamicFun(name: String) {
     Text(text = "Hello $name")
 }
 
+@Composable
+fun InteractFun(inputData: Int, outputEvent: ()-> Unit) {
+    Button(onClick = outputEvent) {
+        Text(text =
+        if (inputData == 0)
+            "Click me, User"
+        else
+            "You(User) clicked me $inputData times"
+        )
+    }
+}
+
 @Preview
 @Composable
-private fun FirstFunPrev() {
+private fun Previews() {
     MyComposePracticeTheme {
+        var clickCount by remember { mutableIntStateOf(0) }
         Column {
             FirstFun()
             DynamicFun(name = "User")
+            InteractFun(inputData = clickCount, outputEvent = {
+                clickCount++
+            })
         }
     }
 }
